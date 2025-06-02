5ad37d217461fc9ee04dd176b223b1eab718dc04
Kannan <kannanpalani455@gmail.com>
2025-06-02 23:12:50 +0530

    add user sqlc code

--- db/query/user.sql
+++ db/query/user.sql
@@ -1,13 +1,13 @@
 -- name: CreateUser :one
-INSERT INTO users (
-  password,
+INSERT INTO "user" (
+  email,
   full_name,
-  email, 
+  password, 
   email_verified
 ) VALUES (
   $1, $2, $3, $4
 ) RETURNING *;
 
 -- name: GetUser :one
-SELECT * FROM users
+SELECT * FROM "user"
 WHERE full_name = $1 LIMIT 1;
